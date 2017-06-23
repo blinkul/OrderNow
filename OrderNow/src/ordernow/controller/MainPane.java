4e683096cca51b96b54bcbd4cb6f15346fe0f5bc
blinkul <anghelciprian91@gmail.com>
2017-06-23 16:00:18 +0300

    Test

---
+++ OrderNow/src/ordernow/controller/MainPane.java
@@ -0,0 +1,15 @@
+package ordernow.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainPane {
+
+	@FXML
+	Button buttonTestID;
+	
+	public void test(){
+		System.out.println("TEST");
+	}
+	
+}
