90114f2c5fe456fee6e3e1d89722a9ef092f0b40
blinkul <anghelciprian91@gmail.com>
2017-06-23 16:15:01 +0300

    PostGit Test

--- OrderNow/src/ordernow/controller/MainPane.java
+++ OrderNow/src/ordernow/controller/MainPane.java
@@ -6,10 +6,14 @@ import javafx.scene.control.Button;
 public class MainPane {
 
 	@FXML
-	Button buttonTestID;
+	Button buttonTestID, aMersGitID;
 	
 	public void test(){
 		System.out.println("TEST");
+	}
+	
+	public void aMersGit(){
+		System.out.println("A mers GIT!!!");
 	}
 	
 }
